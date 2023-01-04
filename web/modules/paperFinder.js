786e54df9901b7a0332f422d4e795b31130be10b
branimir1999 <branimir1999@gmail.com>
2023-01-04 22:35:02 +0100

    Added paperFinder module and expanded HTML functionality

---
+++ web/modules/paperFinder.js
@@ -0,0 +1,131 @@
+(function (PaperFinder) { 
+    // Official name of the module
+    Object.defineProperty(PaperFinder, "name", {
+        value: "PaperFinder",
+        writable: false
+    });
+
+    // Class variables
+    opened = false;
+    findByIdMode = false;
+
+    // HTML elements
+    let findButton = null;       // Button to open the bar  
+    let findBar = null;          // Container for the bar
+    let findTextInput = null;    // Text input for the paper title/ID
+    let findByIdInput = null;    // Checkbox input whether title or ID is used
+    let findSubmitButton = null; // Submit button to start the API call
+
+    // PDF.js objects
+    let textFindBar = null;      // PDFFindBar object used to search text in PDF
+
+    PaperFinder.initialize = function () {
+        if (Global.isNull(Global.app)) {
+            setTimeout(PaperFinder.initialize, 100);
+            return;
+        } else if (Global.isNull(Global.app.findBar)) {
+            setTimeout(PaperFinder.initialize, 100);
+            return;
+        }
+        console.log("Initializing PaperFinder.");
+
+        findButton = document.getElementById('viewPaperFinder');
+        findBar = document.getElementById('paperFinderBar');
+        findTextInput = document.getElementById('paperFinderInput');
+        findByIdInput = document.getElementById('paperFinderById');
+        findSubmitButton = document.getElementById('paperFindSubmit');
+        textFindBar = Global.app.findBar;
+
+        assignListeners();
+    }
+
+    let assignListeners = function () {
+        if (Global.isNull(findButton)) {
+            console.error("HTML div with id 'viewPaperFinder' is null. Cannot assign event listeners.");
+            return;
+        } else if (Global.isNull(findBar)) {
+            console.error("HTML div with id 'paperFinderBar' is null. Cannot assign event listeners.");
+            return;
+        } else if (Global.isNull(findTextInput)) {
+            console.error("HTML div with id 'paperFinderInput' is null. Cannot assign event listeners.");
+            return;
+        } else if (Global.isNull(findByIdInput)) {
+            console.error("HTML div with id 'paperFinderById' is null. Cannot assign event listeners.");
+            return;
+        } else if (Global.isNull(findSubmitButton)) {
+            console.error("HTML div with id 'paperFindSubmit' is null. Cannot assign event listeners.");
+            return;
+        } else if (Global.isNull(textFindBar)) {
+            console.error("findBar object is null. Cannot assign event listeners.");
+            return;
+        }
+
+        // Add event listener to open/close the paper finder bar
+        findButton.addEventListener("click", function () {
+            if (textFindBar.opened) {
+                textFindBar.close();
+            }
+
+            if (opened) {
+                close();
+            } else {
+                open();
+            }
+        });
+
+        // Add event listener to close the text finder bar when paper finder is open
+        textFindBar.toggleButton.addEventListener("click", function () {
+            if (textFindBar.opened && opened) {
+                close();
+            }
+        });
+
+        findTextInput.value = "";
+        findByIdInput.checked = false;
+        findByIdInput.addEventListener("click", function () {
+            // Check is already toggled by the time this event fires
+            if (findByIdInput.checked) {
+                findByIdMode = true;
+                findTextInput.placeholder = "Find on Semantic Scholar by ID...";
+            } else {
+                findByIdMode = false;
+                findTextInput.placeholder = "Find on Semantic Scholar by title...";
+            }
+        });
+
+        // Add submit functionality
+        findSubmitButton.addEventListener("click", function () {
+            find();
+        });
+    }
+
+    let open = function () {
+        if (opened) {
+            return;
+        }
+        opened = true;
+        findButton.classList.add("toggled");
+        findButton.setAttribute("aria-expanded", "true");
+        findBar.classList.remove("hidden");
+    }
+
+    let close = function () {
+        if (!opened) {
+            return;
+        }
+        opened = false;
+        findButton.classList.remove("toggled");
+        findButton.setAttribute("aria-expanded", "false");
+        findBar.classList.add("hidden");
+    }
+
+    let find = function () {
+        if (findByIdMode) {
+            console.log("Finding by Id: " + findTextInput.value)
+        } else {
+            console.log("Finding by title: " + findTextInput.value)
+        }
+    }
+
+    Clippy.addOnLoadEvent(PaperFinder.name, PaperFinder.initialize);
+}(window.Clippy.PaperFinder = window.Clippy.PaperFinder || {}));
